4710ad9023b632767dcd28058e550b4356727c77
Hentci <1222bruce@gmail.com>
2021-08-10 23:54:26 +0800

    [add] slime.js

--- gbfscript/slime.js
+++ gbfscript/slime.js
@@ -8,6 +8,7 @@
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
+
 // 4 fire 5 water 6 7 8 9 dark 3
 // $("#cnt-quest > div.prt-supporter-list.prt-module > div:nth-child(5) > div.btn-supporter.lis-supporter.on > div.prt-button-cover").trigger("tap"); // water
 // $("#cnt-quest > div.prt-supporter-list.prt-module > div:nth-child(4) > div.btn-supporter.lis-supporter.on > div.prt-button-cover").trigger("tap"); // fire
@@ -17,6 +18,77 @@
 // $("#cnt-quest > div.prt-supporter-list.prt-module > div:nth-child(7) > div:nth-child(3) > div.prt-button-cover").trigger("tap");
 // $("#cnt-quest > div.prt-supporter-list.prt-module > div:nth-child(7) > div.btn-supporter.lis-supporter.on > div.prt-button-cover").trigger("tap");
 // $("").trigger("tap");
+
+function restoreConsole() {
+    var i = document.createElement('iframe');
+    i.style.display = 'none';
+    document.body.appendChild(i);
+    window.console = i.contentWindow.console;
+}
+
+function delay(msec) {
+    return new Promise((res) => {
+        setTimeout(res, msec);
+    });
+}
+
+function waitUntilTrue(maxLoop, checker) {
+    return new Promise((resolve, reject) => {
+
+        let loopCount = 0;
+
+        const func = () => {
+            loopCount++;
+
+            if (checker()) {
+                resolve();
+            } else if (loopCount < maxLoop) {
+                setTimeout(func, 100);
+            } else {
+                reject("timeout");
+            }
+        }
+        func();
+    });
+}
+
+function waitOpaqueMask() {
+    const checker = () => !$("#opaque-mask") || ($("#opaque-mask").attr("style") && $("#opaque-mask").attr("style").search("display: none") !== -1);
+
+    return waitUntilTrue(100, checker)
+        .then(() => delay(3000));
+}
+
+async function raidend(maxLoop = 70) {
+    const checkNextBtn = () => $("#wrapper > div.contents > div.cnt-raid > div.prt-command-end").attr("style") && $("#wrapper > div.contents > div.cnt-raid > div.prt-command-end").attr("style").search("display: block") !== -1;
+
+    try {
+        await waitUntilTrue(maxLoop, checkNextBtn);
+        $("#wrapper > div.contents > div.cnt-raid > div.prt-command-end > div").trigger("tap");
+    } catch (reason) {
+        throw `raidend ${reason}`;
+    }
+}
+
+async function doSkill(maxLoop, characterPath, abilityPath) {
+    console.log("doing skill", characterPath, abilityPath);
+
+    const checkCharacter = () => $(characterPath).length === 1;
+    const checkAbility = () => $(abilityPath).length === 1;
+
+    try {
+        await waitUntilTrue(maxLoop, checkCharacter);
+        console.log("click character");
+        $(characterPath).trigger("tap");
+        await delay(3000);
+        await waitUntilTrue(maxLoop, checkAbility);
+        console.log("click ability");
+        $(abilityPath).trigger("tap");
+    } catch (reason) {
+        throw `doSkill ${reason}`;
+    }
+}
+
 function quest() {
     $("#cnt-quest > div.prt-supporter-list.prt-module > div:nth-child(3) > div:nth-child(5) > div.prt-button-cover").trigger("tap");
 
@@ -24,37 +96,48 @@ function quest() {
         $("#wrapper > div.contents > div.pop-deck.supporter > div.prt-btn-deck > div.btn-usual-ok.se-quest-start").trigger("tap");
     }, 3000);
 }
-$("#prt-command-top > div > div > div.lis-character0.btn-command-character.on > img").trigger("tap");
-$("#wrapper > div.contents > div.cnt-raid > div.prt-command > div.prt-command-chara.chara1 > div.prt-ability-list > div.lis-ability.btn-ability-available.on > div.ico-ability562_3.ability-character-num-1-1 > img").trigger("tap");
-$("#wrapper > div.contents > div.cnt-raid > div.prt-command-end > div").trigger("tap");
-$("#prt-command-top > div > div > div.lis-character1.btn-command-character.on > img").trigger("tap");
-$("#wrapper > div.contents > div.cnt-raid > div.prt-command > div.prt-command-chara.chara2 > div.prt-ability-list > div.lis-ability.btn-ability-available.on > div.ico-ability391_1.ability-character-num-2-3 > img").trigger("tap");
-$("#wrapper > div.contents > div.cnt-raid > div.prt-command-end > div").trigger("tap");
-$("").trigger("tap");
-$("").trigger("tap");
+
+// $("#prt-command-top > div > div > div.lis-character0.btn-command-character > img").trigger("tap");
+// $("#wrapper > div.contents > div.cnt-raid > div.prt-command > div.prt-command-chara.chara1 > div.prt-ability-list > div.lis-ability.btn-ability-available > div.ico-ability562_3.ability-character-num-1-1 > img").trigger("tap");
+// $("#wrapper > div.contents > div.cnt-raid > div.prt-command-end > div").trigger("tap");
+// $("#prt-command-top > div > div > div.lis-character1.btn-command-character > img").trigger("tap");
+// $("#wrapper > div.contents > div.cnt-raid > div.prt-command > div.prt-command-chara.chara2 > div.prt-ability-list > div.lis-ability.btn-ability-available > div.ico-ability391_1.ability-character-num-2-3 > img").trigger("tap");
+// $("#wrapper > div.contents > div.cnt-raid > div.prt-command-end > div").trigger("tap");
+// $("").trigger("tap");
 
 function raid() {
-    const checkAttBtn = () => $("#cnt-raid-information > div.btn-attack-start.display-on").length === 1;
+    const skill = [
+        {
+            character: "#prt-command-top > div > div > div.lis-character0.btn-command-character > img",
+            ability: "#wrapper > div.contents > div.cnt-raid > div.prt-command > div.prt-command-chara.chara1 > div.prt-ability-list > div:nth-child(1) > div.ability-character-num-1-1 > img",
+        },
+        {
+            character: "#prt-command-top > div > div > div.lis-character1.btn-command-character > img",
+            ability: "#wrapper > div.contents > div.cnt-raid > div.prt-command > div.prt-command-chara.chara2 > div.prt-ability-list > div:nth-child(3) > div.ability-character-num-2-3 > img",
+        },
+    ];
 
-    const maxLoop = 50;
-    let loopCount = 0;
+    const func = (skillIdx) => {
+        if (skillIdx >= skill.length) return;
 
-    const func = () => {
-        loopCount++;
-
-        if (checkAttBtn()) {
-            $("#cnt-raid-information > div.btn-attack-start.display-on").trigger("tap");
-
-            setTimeout(() => {
-                $("#wrapper > div.contents > div.cnt-raid > div.btn-auto").trigger("tap");
-            }, 1800);
-        } else if (loopCount < maxLoop) {
-            setTimeout(func, 100);
-        }
+        delay(3000)
+            .then(() => waitOpaqueMask())
+            .then(() => {
+                console.log("opaque mask off");
+                return Promise.resolve();
+            })
+            .then(() => doSkill(50, skill[skillIdx].character, skill[skillIdx].ability))
+            .then(() => raidend(100))
+            .then(() => {
+                func(skillIdx + 1);
+            }).catch((reason) => {
+                console.log("raid", reason);
+            });
     }
 
-    func();
+    func(0);
 }
+
 
 function result() {
     const popOK = () => $("#pop > div > div.prt-popup-footer > div").trigger("tap");
@@ -75,14 +158,20 @@ function result() {
 
 function run(last) {
     let l = null;
-    if (last !== "quest" && window.location.hash.search("#quest") !== -1) {
-        quest();
+    if (window.location.hash.search("#quest") !== -1) {
+        if (last !== "quest") {
+            quest();
+        }
         l = "quest";
-    } else if (last !== "raid" && window.location.hash.search("#raid") !== -1) {
-        raid();
+    } else if (window.location.hash.search("#raid") !== -1) {
+        if (last !== "raid") {
+            raid();
+        }
         l = "raid";
-    } else if (last !== "result" && window.location.hash.search("#result") !== -1) {
-        result();
+    } else if (window.location.hash.search("#result") !== -1) {
+        if (last !== "result") {
+            result();
+        }
         l = "result";
     }
 
@@ -90,3 +179,10 @@ function run(last) {
 }
 
 setTimeout(() => run(null), 2000);
+restoreConsole();
+
+window.hentci = {
+    quest,
+    raid,
+    result
+}
