7e851bff969adf6266decd143a5b49ac0b0b7648
Hentci <1222bruce@gmail.com>
2021-08-11 13:08:57 +0800

    [modify] change name

--- windauto.js
+++ windauto.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         dark team (for angel or light)
+// @name         wind team
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
