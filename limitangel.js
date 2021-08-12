d3cf0d2708ad57b8895041d64d6b4c1bc4bd5f66
Hentci <1222bruce@gmail.com>
2021-08-12 16:44:11 +0800

    [modify] change fileName

--- limitangel.js
+++ limitangel.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         slimefarm
+// @name         limitangel
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
