1bf3b03d31a9a1c97635c14bf2bc304c77f913c9
hentci <1222bruce@gmail.com>
2023-06-25 16:00:22 +0800

    [incomplete] four element earth auto

---
+++ four-element/earth_four-element.js
@@ -0,0 +1,111 @@
+// ==UserScript==
+// @name         four-element-earth
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+// 4 fire 5 water 6 7 8 9 dark 3
+// $("").trigger("tap");
+var i = document.createElement('iframe');
+i.style.display = 'none';
+document.body.appendChild(i);
+window.console = i.contentWindow.console;
+function quest() {
+    const checkQueBtn = () => $("#cnt-quest > div.prt-supporter-list.prt-module > div:nth-child(6) > div:nth-child(1) > div.prt-button-cover").length === 1;
+    const maxLoop = 50;
+    let loopCount = 0;
+    const func = () => {
+        loopCount++;
+
+        if(checkQueBtn()) {
+            $("#cnt-quest > div.prt-supporter-list.prt-module > div:nth-child(6) > div:nth-child(1) > div.prt-button-cover").trigger("tap");
+
+            setTimeout(() => {
+                $("#wrapper > div.contents > div.pop-deck.supporter > div.prt-btn-deck > div.btn-usual-ok.se-quest-start").trigger("tap");
+            }, 3000);
+        } else if(loopCount < maxLoop){
+            setTimeout(func, 100);
+        }
+    }
+    
+    func();
+}
+
+function raid() {
+
+    const checkBGBtn = () => $("#wrapper > div.contents > div.cnt-raid > div.prt-start-direction > div.prt-black-bg").length === 1;
+
+    const maxLoop = 50;
+    let loopCount = 0;
+
+    const func = () => {
+        loopCount++;
+
+        if (checkBGBtn()) {
+            $("#wrapper > div.contents > div.cnt-raid > div.prt-start-direction > div.prt-black-bg").trigger("tap");
+
+            // setTimeout(() => {
+            //     $("#wrapper > div.contents > div.cnt-raid > div.btn-auto").trigger("tap");
+            // }, 1800);
+        } else if (loopCount < maxLoop) {
+            setTimeout(func, 100);
+        }
+    }
+
+    func();
+}
+
+function result() {
+    const popOK = () => $("#pop > div > div.prt-popup-footer > div").trigger("tap");
+    const checkPopBtn = () => $("#pop > div > div.prt-popup-footer > div").length > 0;
+    const checkBOSSexistBtn = () => $("#pop > div > div.prt-popup-footer > div.btn-usual-next.on").length > 0;
+    const func = () => {
+        if (checkBOSSexistBtn()){
+            $("#pop > div > div.prt-popup-footer > div.btn-usual-next.on").trigger("tap");
+        }
+        if (checkPopBtn()) {
+            popOK();
+            setTimeout(func, 3000);
+        } else {
+            setTimeout(() => {
+                $("#cnt-result > div.prt-result-cnt > div.prt-button-area.upper > div.btn-retry.cnt-quest").trigger("tap");
+            }, 5000);
+        }
+    }
+
+    func();
+}
+
+function run(last) {
+    let l = null;
+    if (window.location.hash.search("#quest") !== -1) {
+        if (last !== "quest") {
+            quest();
+        }
+        l = "quest";
+    } else if (window.location.hash.search("#raid") !== -1) {
+        if (last !== "raid") {
+            raid();
+        }
+        l = "raid";
+    } else if (window.location.hash.search("#result") !== -1) {
+        if (last !== "result") {
+            result();
+        }
+        l = "result";
+    }
+
+    setTimeout(() => run(l), 2000);
+}
+
+setTimeout(() => run(null), 2000);
+
+window.hentci = {
+    quest, 
+    raid,
+    result
+}
