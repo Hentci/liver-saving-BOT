63ddeafe0fd01c46707bbd0c745034fc167cb778
hentci <1222bruce@gmail.com>
2023-08-15 14:14:12 +0800

    [add] water angel

--- angel/water.js
+++ angel/water.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         earth_angel
+// @name         water_angel
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
