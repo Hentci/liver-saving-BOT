c5dff61c6496928fac095051985761802575b60b
hentci <1222bruce@gmail.com>
2023-08-15 14:11:48 +0800

    [add] earth angel

--- angel/earth.js
+++ angel/earth.js
@@ -39,7 +39,7 @@ function quest() {
         if(checkQueBtn()) {
             // $("#cnt-quest > div.prt-supporter-list.prt-module > div:nth-child(5) > div:nth-child(1) > div.prt-button-cover").trigger("tap");
             var noSpecifySummon = true;
-            for(var i = 1;i <= 15;i++){
+            for(var i = 1;i <= 8;i++){
                 var summonNameElement = document.querySelector("#cnt-quest > div.prt-supporter-list.prt-module > div.prt-supporter-attribute.type3.selected > div:nth-child("+i.toString()+") > div.prt-supporter-info > div.prt-supporter-detail > div.prt-supporter-summon.js-prt-supporter-summon > span.js-summon-name");
                 // 確認元素存在並印出 summon-name 的內容
                 if (summonNameElement) {
