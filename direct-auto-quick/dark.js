ec01aa404154dfee8c39e0221c1c06685acce3df
Hentci <1222bruce@gmail.com>
2024-02-21 14:28:37 +0800

    [modify] start btm

--- direct-auto-quick/dark.js
+++ direct-auto-quick/dark.js
@@ -46,7 +46,7 @@ function quest() {
             }
 
             setTimeout(() => {
-                $("#wrapper > div.contents > div.pop-deck.supporter > div.prt-btn-deck > div.btn-usual-ok.se-quest-start").trigger("tap");
+                $("#wrapper > div.contents > div.pop-deck.supporter > div.prt-btn-deck > div.btn-usual-ok.btn-silent-se").trigger("tap");
             }, 3000);
         } else if(loopCount < maxLoop){
             setTimeout(func, 100);
